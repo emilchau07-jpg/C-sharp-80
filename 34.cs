fd26b59043b97c5cf33a9b5446543ef174f5d821
emilchau07-jpg <emil.chau07@gmail.com>
2026-09-08 20:03:24 +0200

    34.cs

--- 34.cs
+++ 34.cs
@@ -1,2 +1,14 @@
 //Skriv en funktion som läser in och skriver ut ett tecken i taget tills en * skrivs in.
 
+
+static void tecken()
+{
+    string gubbe;
+    do
+    {
+        Console.WriteLine("Ange ett tecken(* avslutar): ");
+        gubbe = Console.ReadLine()!;
+        Console.WriteLine(gubbe);
+    } while (gubbe != "*");
+}
+tecken();
