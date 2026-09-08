d813e19bdc81c6baa42d2c77c0983702a626e361
emilchau07-jpg <emil.chau07@gmail.com>
2026-09-08 19:36:14 +0200

    31.cs

--- 31.cs
+++ 31.cs
@@ -1,2 +1,23 @@
 //Skriv en funktion som läser in ett antal tal och skriver ut dem. Talföljdens slut markeras med 0.
+static void numbers()
+{
+    List<int> numbersInLine = [];
 
+    int tal;
+    do
+    {
+        Console.WriteLine("Ange ett par tal (0 avlsutar): ");
+        tal = int.Parse(Console.ReadLine()!);
+        if (tal != 0)
+        {
+            numbersInLine.Add(tal);
+        }
+    } while (tal != 0);
+
+    foreach (int n in numbersInLine)
+    {
+        Console.WriteLine(n);
+    }
+}
+
+numbers();
